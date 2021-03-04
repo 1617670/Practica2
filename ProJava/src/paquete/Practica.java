da3f6b72480991c2476bd91acfc98d9eda745010
1617670 <ordenadoresmula@gmail.com>
2021-03-04 17:59:57 +0100

    Version 1

---
+++ ProJava/src/paquete/Practica.java
@@ -0,0 +1,11 @@
+package paquete;
+
+public class Practica {
+
+	public static void main(String[] args) {
+		System.out.println("Soy David Valcarcel \ny estoy haciendo la parte dos de la segunda práctica");
+		System.out.println("Hoy es Jueves");
+		System.out.println("Cuatro de Marzo");
+		System.out.println("De 2021");
+	}
+}
